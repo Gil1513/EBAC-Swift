73f1d1285b8e5268641a4cc4a6032512822b6515
Gilmar Da Silva <79201145+Gil1513@users.noreply.github.com>
2023-11-12 05:31:37 -0800

    experiências com Swift

---
+++ modulo 1/ConhecendoPlaygroubg.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var mostra = "mostrar"
+var nao = "nao"
+var str = "Hello, playground"
+print(str)
+
+//MARK? - laco de repeticao
+if str.isEmpty {
+    print(nao)
+}else
+{
+    print(mostra)
+}
